bf2a5119fb42af7bd606c3d77cb5e51bfeb8f13d
Sabarivel M <sabarimuthu290@gmail.com>
2025-09-16 08:45:47 +0530

    Time: 3 ms (5.18%), Space: 44.9 MB (6.24%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int n=t.length();
+        int[][] np=new int[n+1][26];
+        for(int i=0;i<=n;i++){
+            for(int c=0;c<26;c++){
+                np[i][c]=-1;
+            }
+        }
+        for(int c=0;c<26;c++){
+            np[n][c]=-1;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int c=0;c<26;c++){
+                np[i][c]=np[i+1][c];
+            }
+            np[i][t.charAt(i)-'a']=i;
+        }
+        int pos=0;
+        for(char ch:s.toCharArray()){
+            if(pos>n)return false;
+            pos=np[pos][ch-'a'];
+            if(pos==-1)return false;
+            pos++;
+        }
+        return true;
+    }
+}
