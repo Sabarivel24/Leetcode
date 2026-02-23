e3c4bc961f6ace0b31389779d561a63d4d04ff91
Sabarivel M <sabarimuthu290@gmail.com>
2026-02-23 20:15:16 +0530

    Time: 11 ms (69.93%), Space: 50.5 MB (5.36%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,49 +1,32 @@
 class Solution {
-    public int shipWithinDays(int[] weights, int days) {
-        int m=maxweight(weights);
-        int s=sumweight(weights);
-        int l=m;
-        int h=s;
-        while(l<=h){
-            int mid=(l+h)/2;
-            int nd=nodays(weights,mid);
-            if(nd<=days){
-                h=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return l;
-    }
-    public int nodays(int[] weights,int cap){
-        int d=1;
+    public boolean canShip(int[] w,int cap,int days){
         int load=0;
-        for(int i=0;i<weights.length;i++){
-            if((load+weights[i])>cap){
-                d=d+1;
-                load=weights[i];
+        int d=1;
+        for(int x:w){
+            if(load+x>cap){
+                d++;
+                load=0;
+            }
+            load+=x;
+        }
+        return d<=days;
+    }
+    public int shipWithinDays(int[] weights, int days) {
+        int low=0;
+        int high=0;
+        for(int x:weights){
+            low=Math.max(x,low);
+            high+=x;
+        }
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(canShip(weights,mid,days)){
+                high=mid;
             }
             else{
-                load+=weights[i];
+                low=mid+1;
             }
         }
-        return d;
-    }
-    public int maxweight(int[] weights){
-        int max=weights[0];
-        for(int i=1;i<weights.length;i++){
-            if(weights[i]>max){
-                max=weights[i];
-            }
-        }
-        return max;
-    }
-    public int sumweight(int[] weights){
-        int sum=0;
-        for(int i=0;i<weights.length;i++){
-            sum+=weights[i];
-        }
-        return sum;
+        return low;
     }
 }
