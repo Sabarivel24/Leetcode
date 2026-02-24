5d3bc5d9f174a06921c4ebc154a9089714f07a32
Sabarivel M <sabarimuthu290@gmail.com>
2026-02-24 12:57:05 +0530

    Time: 173 ms (5%), Space: 59.5 MB (58.14%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -13,41 +13,42 @@ class Solution {
         if(head==null||head.next==null){
             return head;
         }
-        ListNode mid=getMid(head);
+        ListNode mid=getmid(head);
         ListNode right=mid.next;
         mid.next=null;
-
         ListNode left=sortList(head);
-        ListNode rightt=sortList(right);
-
-        return merge(left,rightt);
-        
+        right=sortList(right);
+        return merge(left,right);
     }
-    private ListNode getMid(ListNode head){
-        ListNode slow=head,fast=head.next;
-        while(fast.next!=null && fast.next.next!=null){
+    public ListNode getmid(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
         }
         return slow;
     }
-    private ListNode merge(ListNode l1,ListNode l2){
-        ListNode dummy=new ListNode();
-        ListNode tail=dummy;
+    public ListNode merge(ListNode l1,ListNode l2){
+        ListNode dummy=new ListNode(-1);
+        ListNode temp=dummy;
         while(l1!=null && l2!=null){
             if(l1.val<l2.val){
-                tail.next=l1;
+                temp.next=l1;
                 l1=l1.next;
             }
             else{
-                tail.next=l2;
+                temp.next=l2;
                 l2=l2.next;
             }
-            tail=tail.next;
+            temp=temp.next;
         }
-        tail.next=(l1!=null)?l1:l2;
+        if(l1!=null){
+            temp.next=l1;
+        }
+        else{
+            temp.next=l2;
+        }
         return dummy.next;
-
-
     }
 }
