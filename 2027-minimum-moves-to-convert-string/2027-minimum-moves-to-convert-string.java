7d9bc7b264a18d68b0943c2baf65d6a709227268
Sabarivel M <sabarimuthu290@gmail.com>
2026-07-21 14:20:40 +0530

    Time: 1 ms (79.63%), Space: 43.1 MB (13.49%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumMoves(String s) {
+        int i=0;
+        int count=0;
+        while(i<s.length()){
+            if(s.charAt(i)=='X'){
+                count++;
+                i=i+3;
+            }
+            else{
+                i++;
+            }
+        }
+        return count;
+    }
+}
